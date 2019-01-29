b05ffee90689a46d200e6fcda909899df08546bf
Daniel Sosunov <dnlsosunov@gmail.com>
2019-01-29 15:17:49 -0500

    Created tests for DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -43,9 +43,9 @@ app.delete('/todos/:id',(req,res)=>{
     var id = req.params.id;
     if(!ObjectID.isValid(id))return res.status(404).send();
 
-    Todo.findOneAndRemove(id).then((todo)=>{
+    Todo.findByIdAndRemove(id).then((todo)=>{
         if(!todo) return res.status(404).send();
-        res.status(200).send(JSON.stringify(todo));
+        res.status(200).send({todo});
     },(e)=>res.status(400).send());
 })
 app.listen(port,()=>{
