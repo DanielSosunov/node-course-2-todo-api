ae255d394abc2d76da9d35d44b8b8dee9f23c696
Daniel Sosunov <dnlsosunov@gmail.com>
2019-01-29 15:36:43 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
-const {ObjectID} = require('mongodb');
 
+const {ObjectID} = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -48,6 +49,28 @@ app.delete('/todos/:id',(req,res)=>{
         res.status(200).send({todo});
     },(e)=>res.status(400).send());
 })
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body}, {new: true}).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+})
+
 app.listen(port,()=>{
     console.log('Started on port:',port)
 });
