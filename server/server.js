e7b2ee54e8a81d7d2bb0cba6d6e60e4db6b632cc
Daniel Sosunov <dnlsosunov@gmail.com>
2019-01-30 14:54:21 -0500

    Setup separate Test and Development Environments

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
@@ -8,7 +10,7 @@ const bodyParser = require('body-parser');
 const _ = require('lodash');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
